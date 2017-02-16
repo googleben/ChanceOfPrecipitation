b7b313d2b094a5261b8b57578c78ebc96e6431ec
Antidisestablishmentarianism <saifkahmad@gmail.com>
2017-02-16 13:17:07 -0600

    Add GameObject class

---
+++ ChanceOfPrecipitation/ChanceOfPrecipitation/GameObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ChanceOfPrecipitation
+{
+    public abstract class GameObject
+    {
+        public bool toDestroy { get; private set; }
+
+        public void Destroy()
+        {
+            toDestroy = true;
+        }
+
+        public abstract void Update(IEnumerable<GameObject> objects);
+        public abstract void Draw(SpriteBatch sb);
+    }
+}
