cb23f4d22eee9d11164fda57faa6f0495bc4528a
Ben Wyatt <googleben@sbcglobal.net>
2017-02-23 13:41:32 -0600

    Add Sources Dictionary

--- ChanceOfPrecipitation/ChanceOfPrecipitation/TextureManager.cs
+++ ChanceOfPrecipitation/ChanceOfPrecipitation/TextureManager.cs
@@ -10,10 +10,12 @@ namespace ChanceOfPrecipitation
     public static class TextureManager
     {
         public static Dictionary<string, Texture2D> Textures;
+        public static Dictionary<string, Rectangle> Sources;
 
         static TextureManager()
         {
             Textures = new Dictionary<string, Texture2D>();
+            Sources = new Dictionary<string, Rectangle>();
         }
     }
 }
