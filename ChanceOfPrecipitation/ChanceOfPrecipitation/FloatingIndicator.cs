6250f4371b2c0cebdf4d6063e1478840b0958e68
Saif Ahmad <180502@ads.friscoisd.org>
2017-02-23 13:43:20 -0600

    Begin floating indicators

---
+++ ChanceOfPrecipitation/ChanceOfPrecipitation/FloatingIndicator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ChanceOfPrecipitation
+{
+    class FloatingIndicator : GameObject
+    {
+        private Vector2 position;
+        private float upSpeed;
+        private float oscillationDist, oscillationSpeed;
+
+        private Color color;
+
+        public FloatingIndicator()
+        {
+
+        }
+
+        public override void Update(IEnumerable<GameObject> objects)
+        {
+             
+        }
+
+        public override void Draw(SpriteBatch sb)
+        {
+            
+        }
+    }
+}
