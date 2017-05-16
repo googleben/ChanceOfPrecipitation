90e3fb31050598ea984065e3bff08b098a450743
Antidisestablishmentarianism <saifkahmad@gmail.com>
2017-05-16 14:27:46 -0500

    Start HUD

---
+++ ChanceOfPrecipitation/ChanceOfPrecipitation/HUD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ChanceOfPrecipitation
+{
+    public class HUD
+    {
+        public HUD(Ability one, Ability two, Ability three, Ability four)
+        {
+
+        }
+    }
+}
