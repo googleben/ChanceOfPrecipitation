70c57d93b933aebe4b91a56710843bc7b7629f3e
Antidisestablishmentarianism <saifkahmad@gmail.com>
2017-05-22 14:27:41 -0500

    Add new TextureManager dictionary

--- ChanceOfPrecipitation/ChanceOfPrecipitation/TextureManager.cs
+++ ChanceOfPrecipitation/ChanceOfPrecipitation/TextureManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace ChanceOfPrecipitation
@@ -7,11 +8,13 @@ namespace ChanceOfPrecipitation
     {
         public static Dictionary<string, Texture2D> textures;
         public static Dictionary<string, TextureInfo> blocks;
+        public static Dictionary<Type, TextureInfo> abilities;
 
         static TextureManager()
         {
             textures = new Dictionary<string, Texture2D>();
             blocks = new Dictionary<string, TextureInfo>();
+            abilities = new Dictionary<Type, TextureInfo>();
         }
     }
 }
